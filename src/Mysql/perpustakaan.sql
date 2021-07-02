8202545f91d1fec33a745ba43fc67e1034845f03
reyestaaa <muhammadadrianreyesta9d@gmail.com>
2021-07-02 23:52:11 +0700

    Perpustakaan

---
+++ src/Mysql/perpustakaan.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 01, 2021 at 08:26 AM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 8.0.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `perpustakaan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `buku`
+--
+
+CREATE TABLE `buku` (
+  `id_buku` int(50) NOT NULL,
+  `judul` varchar(35) NOT NULL,
+  `jenis` varchar(35) NOT NULL,
+  `pengarang` varchar(35) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `buku`
+--
+
+INSERT INTO `buku` (`id_buku`, `judul`, `jenis`, `pengarang`) VALUES
+(1, 'testing', 'Sejarah', 'Adrian Reyesta'),
+(12121, 'ffmfkf', 'Sejarah', 'kdakdakdkd');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `buku`
+--
+ALTER TABLE `buku`
+  ADD PRIMARY KEY (`id_buku`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `buku`
+--
+ALTER TABLE `buku`
+  MODIFY `id_buku` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12132;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ src/SrcTugas/Koneksi.java
@@ -0,0 +1,30 @@
+package SrcTugas;
+
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author PY7
+ */
+public class Koneksi {
+    private static java.sql.Connection koneksi;
+    
+    public static java.sql.Connection getKoneksi(){
+        if (koneksi == null){
+            try {
+                String url = "jdbc:mysql://localhost:3306/perpustakaan";
+                String user = "root";
+                String password = "";
+                DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+                koneksi = DriverManager.getConnection(url, user, password);
+                System.out.println("Berhasil Terhubung");
+            } catch (Exception e) {
+                System.out.println("Error");
+            }
+        }
+        return koneksi;
+    }
+    public static void main(String args[]){
+        getKoneksi();
+    }   
+}
